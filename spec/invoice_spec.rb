d441196edcf697f688f8d7a6113e1d73262f340b
Édouard Brière <edouard.briere@gmail.com>
2022-02-02 15:58:35 +0100

    Fix Style/StringLiterals offence

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -1,74 +1,74 @@
-require "spec_helper"
+require 'spec_helper'
 
 module Payday
   describe Invoice do
 
-    it "should be able to be initalized with a hash of options" do
-      i = Invoice.new(invoice_number: 20, bill_to: "Here", ship_to: "There",
-          notes: "These are some notes.",
+    it 'should be able to be initalized with a hash of options' do
+      i = Invoice.new(invoice_number: 20, bill_to: 'Here', ship_to: 'There',
+          notes: 'These are some notes.',
           line_items:
-            [LineItem.new(price: 10, quantity: 3, description: "Shirts")],
-          shipping_rate: 15.00, shipping_description: "USPS Priority Mail:",
-          tax_rate: 0.125, tax_description: "Local Sales Tax, 12.5%",
+            [LineItem.new(price: 10, quantity: 3, description: 'Shirts')],
+          shipping_rate: 15.00, shipping_description: 'USPS Priority Mail:',
+          tax_rate: 0.125, tax_description: 'Local Sales Tax, 12.5%',
           invoice_date: Date.civil(1993, 4, 12))
 
       expect(i.invoice_number).to eq(20)
-      expect(i.bill_to).to eq("Here")
-      expect(i.ship_to).to eq("There")
-      expect(i.notes).to eq("These are some notes.")
-      expect(i.line_items[0].description).to eq("Shirts")
-      expect(i.shipping_rate).to eq(BigDecimal("15.00"))
-      expect(i.shipping_description).to eq("USPS Priority Mail:")
-      expect(i.tax_rate).to eq(BigDecimal("0.125"))
-      expect(i.tax_description).to eq("Local Sales Tax, 12.5%")
+      expect(i.bill_to).to eq('Here')
+      expect(i.ship_to).to eq('There')
+      expect(i.notes).to eq('These are some notes.')
+      expect(i.line_items[0].description).to eq('Shirts')
+      expect(i.shipping_rate).to eq(BigDecimal('15.00'))
+      expect(i.shipping_description).to eq('USPS Priority Mail:')
+      expect(i.tax_rate).to eq(BigDecimal('0.125'))
+      expect(i.tax_description).to eq('Local Sales Tax, 12.5%')
       expect(i.invoice_date).to eq(Date.civil(1993, 4, 12))
     end
 
-    it "should total all of the line items into a subtotal correctly" do
+    it 'should total all of the line items into a subtotal correctly' do
       i = Invoice.new
 
       # $100 in Pants
-      i.line_items << LineItem.new(price: 20, quantity: 5, description: "Pants")
+      i.line_items << LineItem.new(price: 20, quantity: 5, description: 'Pants')
 
       # $30 in Shirts
       i.line_items <<
-        LineItem.new(price: 10, quantity: 3, description: "Shirts")
+        LineItem.new(price: 10, quantity: 3, description: 'Shirts')
 
       # $1000 in Hats
-      i.line_items << LineItem.new(price: 5, quantity: 200, description: "Hats")
+      i.line_items << LineItem.new(price: 5, quantity: 200, description: 'Hats')
 
-      expect(i.subtotal).to eq(BigDecimal("1130"))
+      expect(i.subtotal).to eq(BigDecimal('1130'))
     end
 
-    it "should calculate the correct tax rounded to two decimal places" do
+    it 'should calculate the correct tax rounded to two decimal places' do
       i = Invoice.new(tax_rate: 10.0)
-      i.line_items << LineItem.new(price: 20, quantity: 5, description: "Pants")
+      i.line_items << LineItem.new(price: 20, quantity: 5, description: 'Pants')
 
-      expect(i.tax).to eq(BigDecimal("10"))
+      expect(i.tax).to eq(BigDecimal('10'))
     end
 
     it "shouldn't apply taxes to invoices with subtotal <= 0" do
       i = Invoice.new(tax_rate: 10.0)
       i.line_items << LineItem.new(price: -1, quantity: 100,
-        description: "Negative Priced Pants")
+        description: 'Negative Priced Pants')
 
-      expect(i.tax).to eq(BigDecimal("-10"))
+      expect(i.tax).to eq(BigDecimal('-10'))
     end
 
-    it "should calculate the total for an invoice correctly" do
+    it 'should calculate the total for an invoice correctly' do
       i = Invoice.new(tax_rate: 10.0)
 
       # $100 in Pants
-      i.line_items << LineItem.new(price: 20, quantity: 5, description: "Pants")
+      i.line_items << LineItem.new(price: 20, quantity: 5, description: 'Pants')
 
       # $30 in Shirts
       i.line_items <<
-        LineItem.new(price: 10, quantity: 3, description: "Shirts")
+        LineItem.new(price: 10, quantity: 3, description: 'Shirts')
 
       # $1000 in Hats
-      i.line_items << LineItem.new(price: 5, quantity: 200, description: "Hats")
+      i.line_items << LineItem.new(price: 5, quantity: 200, description: 'Hats')
 
-      expect(i.total).to eq(BigDecimal("1243"))
+      expect(i.total).to eq(BigDecimal('1243'))
     end
 
     it "is overdue when it's past date and unpaid" do
@@ -81,8 +81,8 @@ module Payday
       expect(i.overdue?).not_to eq(true)
     end
 
-    it "is overdue when due date is a time before the current date" do
-      i = Invoice.new(due_at: Time.parse("Jan 1 14:33:20 GMT 2011"))
+    it 'is overdue when due date is a time before the current date' do
+      i = Invoice.new(due_at: Time.parse('Jan 1 14:33:20 GMT 2011'))
       expect(i.overdue?).to eq(true)
     end
 
@@ -91,7 +91,7 @@ module Payday
       expect(i.refunded?).not_to eq(true)
     end
 
-    it "should be refunded when marked as refunded" do
+    it 'should be refunded when marked as refunded' do
       i = Invoice.new(refunded_at: Date.today)
       expect(i.refunded?).to eq(true)
     end
@@ -101,12 +101,12 @@ module Payday
       expect(i.paid?).not_to eq(true)
     end
 
-    it "should be paid when marked as paid" do
+    it 'should be paid when marked as paid' do
       i = Invoice.new(paid_at: Date.today)
       expect(i.paid?).to eq(true)
     end
 
-    it "should be able to iterate over details" do
+    it 'should be able to iterate over details' do
       i = Invoice.new(invoice_details: [%w(Test Yes), %w(Awesome Absolutely)])
       details = []
       i.each_detail do |key, value|
@@ -118,9 +118,9 @@ module Payday
       expect(details).to include(%w(Awesome Absolutely))
     end
 
-    it "should be able to iterate through invoice_details as a hash" do
+    it 'should be able to iterate through invoice_details as a hash' do
       i = Invoice.new(invoice_details:
-        { "Test" => "Yes", "Awesome" => "Absolutely" })
+        { 'Test' => 'Yes', 'Awesome' => 'Absolutely' })
       details = []
       i.each_detail do |key, value|
         details << [key, value]
@@ -131,34 +131,34 @@ module Payday
       expect(details).to include(%w(Awesome Absolutely))
     end
 
-    describe "rendering" do
+    describe 'rendering' do
       before do
-        Dir.mkdir("tmp") unless File.exist?("tmp")
+        Dir.mkdir('tmp') unless File.exist?('tmp')
         Config.default.reset
       end
 
       let(:invoice) { new_invoice(invoice_params) }
       let(:invoice_params) { {} }
 
-      it "should render to a file" do
-        File.unlink("tmp/testing.pdf") if File.exist?("tmp/testing.pdf")
+      it 'should render to a file' do
+        File.unlink('tmp/testing.pdf') if File.exist?('tmp/testing.pdf')
 
-        invoice.render_pdf_to_file("tmp/testing.pdf")
+        invoice.render_pdf_to_file('tmp/testing.pdf')
 
-        expect(File.exist?("tmp/testing.pdf")).to be true
+        expect(File.exist?('tmp/testing.pdf')).to be true
       end
 
-      context "with some invoice details" do
+      context 'with some invoice details' do
         let(:invoice_params) do
           {
             invoice_details: {
-              "Ordered By:" => "Alan Johnson",
-              "Paid By:" => "Dude McDude"
+              'Ordered By:' => 'Alan Johnson',
+              'Paid By:' => 'Dude McDude'
             }
           }
         end
 
-        it "should render an invoice correctly" do
+        it 'should render an invoice correctly' do
           Payday::Config.default.company_details = <<-EOF
             10 This Way
             Manhattan, NY 10001
@@ -167,38 +167,38 @@ module Payday
           EOF
 
           invoice.line_items += [
-            LineItem.new(price: 20, quantity: 5, description: "Pants"),
-            LineItem.new(price: 10, quantity: 3, description: "Shirts"),
-            LineItem.new(price: 5, quantity: 200, description: "Hats")
+            LineItem.new(price: 20, quantity: 5, description: 'Pants'),
+            LineItem.new(price: 10, quantity: 3, description: 'Shirts'),
+            LineItem.new(price: 5, quantity: 200, description: 'Hats')
           ] * 30
 
-          expect(invoice.render_pdf).to match_binary_asset "testing.pdf"
+          expect(invoice.render_pdf).to match_binary_asset 'testing.pdf'
         end
       end
 
-      context "paid, with an svg logo" do
+      context 'paid, with an svg logo' do
         before do
-          logo = { filename: "spec/assets/tiger.svg", size: "100x100" }
+          logo = { filename: 'spec/assets/tiger.svg', size: '100x100' }
           Payday::Config.default.invoice_logo = logo
         end
 
         let(:invoice_params) { { paid_at: Date.civil(2012, 2, 22) } }
 
-        it "should render an invoice correctly" do
+        it 'should render an invoice correctly' do
           invoice.line_items += [
-            LineItem.new(price: 20, quantity: 5, description: "Pants"),
-            LineItem.new(price: 10, quantity: 3, description: "Shirts"),
-            LineItem.new(price: 5, quantity: 200.0, description: "Hats")
+            LineItem.new(price: 20, quantity: 5, description: 'Pants'),
+            LineItem.new(price: 10, quantity: 3, description: 'Shirts'),
+            LineItem.new(price: 5, quantity: 200.0, description: 'Hats')
           ] * 3
 
-          expect(invoice.render_pdf).to match_binary_asset "svg.pdf"
+          expect(invoice.render_pdf).to match_binary_asset 'svg.pdf'
         end
       end
 
       def new_invoice(params = {})
         default_params = {
           tax_rate: 0.1,
-          notes: "These are some crazy awesome notes!",
+          notes: 'These are some crazy awesome notes!',
           invoice_number: 12,
           invoice_date: Date.civil(2011, 1, 1),
           due_at: Date.civil(2011, 1, 22),
--- spec/pdf_renderer_spec.rb
+++ spec/pdf_renderer_spec.rb
@@ -1,10 +1,10 @@
-require "spec_helper"
+require 'spec_helper'
 
 module Payday
   describe PdfRenderer do
-    describe ".number_to_currency" do
-      it "formats numbers to currency currectly" do
-        expect(PdfRenderer.number_to_currency(20, nil)).to eq("$20.00")
+    describe '.number_to_currency' do
+      it 'formats numbers to currency currectly' do
+        expect(PdfRenderer.number_to_currency(20, nil)).to eq('$20.00')
       end
     end
   end
