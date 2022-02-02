a9e1c1a12f3dce0879483b131ef509f4cdc7657b
Édouard Brière <edouard.briere@gmail.com>
2022-02-02 16:50:10 +0100

    Fix rubocop-rspec offences

--- spec/pdf_renderer_spec.rb
+++ spec/pdf_renderer_spec.rb
@@ -4,7 +4,7 @@ module Payday
   describe PdfRenderer do
     describe '.number_to_currency' do
       it 'formats numbers to currency currectly' do
-        expect(PdfRenderer.number_to_currency(20, nil)).to eq('$20.00')
+        expect(described_class.number_to_currency(20, nil)).to eq('$20.00')
       end
     end
   end
